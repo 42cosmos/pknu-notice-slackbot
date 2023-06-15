029c5f4df61b43f49712585dc9efda843a89002e
Eunbin Park <parkeb417@gmail.com>
2023-06-15 19:23:45 +0900

    Check if the message was previously sent

--- crawling.py
+++ crawling.py
@@ -1,3 +1,5 @@
+import os.path
+
 from selenium import webdriver
 
 from selenium.webdriver.common.by import By
@@ -11,6 +13,27 @@ class NoticeRow:
     text: str
     update_date: str
     link_to_notice: str
+
+
+def write_page_id_to_file(href: str):
+    if os.path.exists("./page_id.txt") is False:
+        with open("./page_id.txt", "w") as f:
+            f.write(f"\n{href}")
+        return
+
+    with open("page_id.txt", "a") as f:
+        f.write(f"\n{href}")
+
+
+def read_page_id_from_file(href: str):
+    if os.path.exists("./page_id.txt") is False:
+        return False
+
+    with open("./page_id.txt", "r") as f:
+        page_id_list = f.read().strip().split()
+        if href in page_id_list:
+            return True
+        return False
 
 
 def get_table_rows(row):
@@ -44,8 +67,14 @@ def make_slack_format(gradudate: bool, notice: NoticeRow) -> dict:
         "title_link": notice.link_to_notice,
         "footer": f"{school_name}  |  {notice.update_date}",
     }
-
     return slack_text
+
+
+def send_slack_message(slack, notice, gradudate):
+    previously_sent = read_page_id_from_file(notice.link_to_notice)
+    if not previously_sent:
+        slack.alarm_msg(make_slack_format(gradudate=gradudate, notice=notice))
+        write_page_id_to_file(notice.link_to_notice)
 
 
 if __name__ == "__main__":
@@ -60,11 +89,11 @@ if __name__ == "__main__":
     graduate_notice = main(driver, graduate_url)
 
     for notice in graduate_notice:
-        slack.alarm_msg(make_slack_format(gradudate=True, notice=notice))
+        send_slack_message(slack, notice, gradudate=True)
 
     # 학부
     undergraduate_url = "https://sme.pknu.ac.kr/sme/721"
     undergraduate_notice = main(driver, undergraduate_url)
 
     for notice in undergraduate_notice:
-        slack.alarm_msg(make_slack_format(gradudate=False, notice=notice))
+        send_slack_message(slack, notice, gradudate=False)
